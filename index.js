6aae469956e19b975abd2ebba9412f923694cb8a
NazmulAyyan <rnazmul2424@gmail.com>
2023-05-15 12:17:18 +0600

    blog.json file is updated

--- index.js
+++ index.js
@@ -3,19 +3,36 @@ const app = express();
 const cors = require('cors');
 const port = process.env.PORT || 5000;
 
+//chefs
 const chefs = require('./data/chefs.json');
-
+//blogs
+const blogs = require('./data/blogs.json');
 
 app.use(cors());
-
+app.use(express.json());
+//chefs
 app.get('/', (req, res) => {
     res.send('chef is coming');
 });
+//blogs
+// app.get('/', (req, res) =>{
+//     res.send('blog is coming')
+// })
 
+//chefs
 app.get('/chefs', (req, res) =>{
     res.send(chefs)
+})
+//blogs
+app.get('/blogs', (req, res) =>{
+    res.send(blogs)
 })
 
 app.listen(port, () => {
     console.log(`chef api is coming on port: ${port}`);
 });
+
+//blogs
+// app.listen(port,() =>{
+//     console.log(`blogs api is coming on port: ${port}`);
+// })
